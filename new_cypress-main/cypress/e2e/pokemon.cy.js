90859acd6b3109654cb5147e9842f6b0871fc62a
Niki <koniand@mail.ru>
2025-04-24 22:34:44 +0300

    Add files via upload

--- new_cypress-main/cypress/e2e/pokemon.cy.js
+++ new_cypress-main/cypress/e2e/pokemon.cy.js
@@ -1,8 +1,8 @@
 describe('Проверка покупки нового аватара', function () {                 
     it('e2e тест на покупку нового аватара для тренера', function () {   
          cy.visit('https://pokemonbattle.ru/');                          // заходим на сайт
-         cy.get('input[id="k_email"]').type('koschceev.nikita@yandex.ru');                   // вводим логин
-         cy.get('input[id="k_password"]').type('Pokemoon1718');               // вводим пароль
+         cy.get('input[id="k_email"]').type('USER_LOGIN');                   // вводим логин
+         cy.get('input[id="k_password"]').type('USER_PASSWORD');               // вводим пароль
          cy.get('button[type="submit"]').click();                // нажимаем кнопку Подтвердить
          cy.wait(2000);
          cy.get('.header_card_trainer').click();            // Клик в шапке на аву тренера
